ca221d17017a6cd3dc13564263fad9c6ec1a92e2
Maynor Alejandro Bolaños Orellana <1925597@clases.edu.sv>
2025-09-09 11:36:07 -0600

    Conexion a la db

---
+++ src/routes/zoneRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const zonaController = require("../controllers/zoneController");
+
+router.get("/", zonaController.getAllZonas);
+
+router.get("/:id", zonaController.getZonaById);
+
+router.post("/", zonaController.createZona);
+
+router.put("/:id", zonaController.updateZona);
+
+router.delete("/:id", zonaController.deleteZona);
+
+module.exports = router;
---
+++ src/schemas/zoneSchema.js
@@ -0,0 +1,6 @@
+const zonaSchema = {
+  nombre: { type: "string", required: true },
+  descripcion: { type: "string", required: false }, 
+};
+
+module.exports = zonaSchema;
